17e2bc181ac09c035582aebcd33bd9d6eeef123e
codejuandedios <codejuandedios@gmail.com>
2019-09-26 16:31:07 -0600

    FUNCIONAL ENTRAR A LA CAMARA
    Entra a la camara pero tira un error al momento de guardarla en galeria :(

--- app/src/main/java/com/example/fixit/Reporte_Usuario.java
+++ app/src/main/java/com/example/fixit/Reporte_Usuario.java
@@ -2,21 +2,99 @@ package com.example.fixit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+
 import android.widget.Spinner;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Reporte_Usuario extends AppCompatActivity {
 
-    Spinner Opciones;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "MainActivity";
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
+
+    Spinner spinner;
+    Spinner spinneredifi;
+    Spinner spinnerSal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte__usuario);
-        Opciones=(Spinner) findViewById(R.id.spinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource( this, R.array.Opciones, android.R.layout.simple_spinner_item);
-        Opciones.setAdapter(adapter);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        String[] Problema = {"Elija su problema","Opción 1","Opción 2","Opción 3","Opción 4"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Problema));
+
+        spinneredifi = (Spinner) findViewById(R.id.spinnerEdificio);
+        String[] Edificios = {"Elija el Edificio","Torre 1","Torre 2","Torre 3","Torre 4"};
+        spinneredifi.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Edificios));
+
+        spinnerSal = (Spinner) findViewById(R.id.spinnerSalon);
+        String[] Salones = {"Elija el Salon","Salon 1","Salon 2","Salon 3","Salon 4"};
+        spinnerSal.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Salones));
+    }
+
+    public void onclick(View view) {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(cameraIntent);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.i(TAG, "IOException");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  // prefix
+                ".jpg",         // suffix
+                storageDir      // directory
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                mImageView.setImageBitmap(mImageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
