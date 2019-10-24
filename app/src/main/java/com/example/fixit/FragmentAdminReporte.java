bf81215aa0ff3661b0e43b0d1446b8100d6fcccc
codejuandedios <codejuandedios@gmail.com>
2019-10-23 19:16:41 -0600

    cambios interfaz usuarioadmin

--- app/src/main/java/com/example/fixit/FragmentAdminReporte.java
+++ app/src/main/java/com/example/fixit/FragmentAdminReporte.java
@@ -57,7 +57,7 @@ public class FragmentAdminReporte extends Fragment {
 
     public Connection conexionBD(){
         Connection conexion = null;
-        String host = "192.168.1.27";
+        String host = "192.168.1.13";
         String port = "3306";
         String dbName = "fixit";
         String userName = "root";
--- app/src/main/java/com/example/fixit/FragmentAdminUsuarios.java
+++ app/src/main/java/com/example/fixit/FragmentAdminUsuarios.java
@@ -8,12 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentAdminUsuarios extends Fragment {
+
+    View vistaR;
+    private EditText buscarRol;
+    private Button btnBuscarRol;
+    private EditText txtCarne;
+    private EditText txtCorreo;
+    private EditText txtNombre;
+    private EditText txtApellido;
+    private Spinner spinnerRol;
+    private Spinner spinnerEstado;
+    ;
 
 
     public FragmentAdminUsuarios() {
@@ -25,7 +40,34 @@ public class FragmentAdminUsuarios extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_admin_usuarios, container, false);
-    }
+        vistaR = inflater.inflate(R.layout.fragment_fragment_admin_usuarios, container, false);
+        spinnerRol = (Spinner) vistaR.findViewById(R.id.spinnerRol);
+        spinnerEstado = (Spinner) vistaR.findViewById(R.id.spinnerEstado);
+        buscarRol = (EditText) vistaR.findViewById(R.id.txtBuscarCarne);
+        btnBuscarRol = (Button) vistaR.findViewById(R.id.btnBuscarCarne);
+        txtCarne = (EditText) vistaR.findViewById(R.id.txtcarneRol);
+        txtCorreo = (EditText) vistaR.findViewById(R.id.txtCorreoRol);
+        txtNombre = (EditText) vistaR.findViewById(R.id.txtNombreRol);
+        txtApellido = (EditText) vistaR.findViewById(R.id.txtApellidoRol);
 
+
+        /* String [] values =
+                {"Selecine el rol","1. Usuario","2. Moderador","3. Administrador"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, values);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinnerRol.setAdapter(adapter);
+        */
+        //__________Spinner de ROl____________
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.TipodeRolUsuario, android.R.layout.simple_spinner_item);
+        spinnerRol.setAdapter(adapter);
+
+        //__________Spinner de Estado_______
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.EstadoUsuario, android.R.layout.simple_spinner_item);
+        spinnerEstado.setAdapter(adapter1);
+
+        return vistaR;
+    }
 }
